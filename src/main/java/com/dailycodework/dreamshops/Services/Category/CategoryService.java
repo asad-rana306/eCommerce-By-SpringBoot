856ea73ce72d3e9e61b1c1250d1dcd9fd8f005f0
Rana Asad <asadcodecraft@gmail.com>
2025-04-13 23:19:44 +0500

    making Category class and done with code

---
+++ src/main/java/com/dailycodework/dreamshops/Services/Category/CategoryService.java
@@ -0,0 +1,49 @@
+package com.dailycodework.dreamshops.Services.Category;
+
+import com.dailycodework.dreamshops.exceptions.ResourceNotFoundException;
+import com.dailycodework.dreamshops.model.Category;
+import com.dailycodework.dreamshops.repository.CategoryRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.boot.context.config.ConfigDataResourceNotFoundException;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+@RequiredArgsConstructor
+public class CategoryService implements ICategoryService {
+
+    private final CategoryRepository categoryRepository;
+    @Override
+    public Category getCategoryById(Long id) {
+        return categoryRepository.findById(id)
+                .orElseThrow(()-> new ResourceNotFoundException("Category not found"));
+    }
+
+    @Override
+    public Category getCategoryByName(String name) {
+        return categoryRepository.findByName(name);
+    }
+
+    @Override
+    public List<Category> getAllCategories() {
+        return List.of();
+    }
+
+    @Override
+    public Category addCategory(Category category) {
+        return null;
+    }
+
+    @Override
+    public Category updateCategory(Category category) {
+        return null;
+    }
+
+    @Override
+    public void deleteCategory(Long id) {
+        categoryRepository.findById(id).ifPresentOrElse(categoryRepository::delete,()->{
+            throw new ResourceNotFoundException("Category not found");
+        });
+    }
+}
