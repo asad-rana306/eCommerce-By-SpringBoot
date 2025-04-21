f73d97f2a8df7aa82a0812d676de5653578d71e7
Rana Asad <asadcodecraft@gmail.com>
2025-04-21 16:38:59 +0500

    writing code in ProductController.java

--- src/main/java/com/dailycodework/dreamshops/Controller/ProductController.java
+++ src/main/java/com/dailycodework/dreamshops/Controller/ProductController.java
@@ -1,9 +1,40 @@
 package com.dailycodework.dreamshops.Controller;
 
+import com.dailycodework.dreamshops.Services.ProductService.IProductService;
+import com.dailycodework.dreamshops.exceptions.ResourceNotFoundException;
+import com.dailycodework.dreamshops.model.Product;
+import com.dailycodework.dreamshops.response.ApiResponse;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.List;
+
+import static org.springframework.http.HttpStatus.NOT_FOUND;
+
+@RequiredArgsConstructor
 @RestController
-@RequestMapping("${}/image")
+@RequestMapping("${api.prefix}/products")
 public class ProductController {
+    private final IProductService productService;
+
+    @GetMapping("/all")
+    public ResponseEntity<ApiResponse> getAllProducts(){
+        List<Product> products = productService.getAllProducts();
+        return ResponseEntity.ok(new ApiResponse("Products fetched successfully", products));
+    }
+
+    @GetMapping("product/{productId}/product")
+    public ResponseEntity<ApiResponse> getProductById(@PathVariable Long productId){
+        try {
+            Product product = productService.getProductById(productId);
+            return ResponseEntity.ok(new ApiResponse("Product fetched successfully", product));
+        } catch (ResourceNotFoundException e) {
+            return ResponseEntity.status(NOT_FOUND)
+                    .body(new ApiResponse(e.getMessage(), null));
+        }
+    }
 }
